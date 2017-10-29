4747a27295ad6a67288e8715d02622a84981eaa4
skadaveru3 <skadaveru3@gatech.edu>
2017-10-28 21:28:09 -0400

    added table view cells

---
+++ HomeworkManagement/AssignmentCell.swift
@@ -0,0 +1,32 @@
+//
+//  AssignmentCell.swift
+//  HomeworkManagement
+//
+//  Created by Sathvik Kadaveru on 10/28/17.
+//  Copyright © 2017 FreshMac. All rights reserved.
+//
+
+import UIKit
+
+class AssignmentCell: UITableViewCell {
+    
+    
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var startLabel: UILabel!
+    @IBOutlet weak var endLabel: UILabel!
+    
+    @IBOutlet weak var progressBar: UIProgressView!
+    @IBOutlet weak var percentLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
