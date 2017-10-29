45850e90c3d9d7d5ddef9f0642a6adb5b85e8279
Jerome Belcher <schoolxmanagement@gmail.com>
2017-10-28 21:38:23 -0400

    Drop Down Dates

---
+++ HomeworkManagement/AddAssignmentViewController.swift
@@ -0,0 +1,39 @@
+//
+//  AddAssignmentViewController.swift
+//  HomeworkManagement
+//
+//  Created by FreshMac on 10/28/17.
+//  Copyright © 2017 FreshMac. All rights reserved.
+//
+
+import UIKit
+
+class AddAssignmentViewController: UIViewController {
+    @IBOutlet weak var textBox1: UITextField!
+    @IBOutlet weak var dropDown1: UIPickerView!
+    @IBOutlet weak var textBox2: UITextField!
+    @IBOutlet weak var dropDown2: UIPickerView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
